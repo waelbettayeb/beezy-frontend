11a8ff92c865ce8bc060fa7ec3f6794dbfd91a1c
WaelMohamedKhelil <khelilwael@gmail.com>
2020-07-06 15:54:25 +0100

    add hero component

---
+++ components/molecules/Hero/Hero.tsx
@@ -0,0 +1,71 @@
+import React, { ReactElement } from "react";
+import { Grid, Cell, ALIGNMENT } from "baseui/layout-grid";
+import { Label1, Display2, Paragraph1, Display4 } from "baseui/typography";
+import { useStyletron } from "baseui";
+import { Search } from "baseui/icon";
+import { Input } from "baseui/input";
+import { StyledLink } from "baseui/link";
+
+interface Props {}
+
+function Hero({}: Props): ReactElement {
+  const textString = "We ignite bee hives by companying beekeepers.";
+  const bodyString =
+    "Good things happen when people can connect, find the right bee yard when your hives can produce more. ";
+  function After() {
+    const [css, theme] = useStyletron();
+    return (
+      <div
+        className={css({
+          display: "flex",
+          alignItems: "center",
+          paddingRight: theme.sizing.scale500,
+        })}
+      >
+        <Search size="18px" />
+      </div>
+    );
+  }
+  return (
+    <Grid
+      align={ALIGNMENT.center}
+      gridUnit="rem"
+      gridGutters={[1, 2, 8]}
+      gridMargins={[1, 2, 6]}
+    >
+      <Cell span={[4, 4, 6]}>
+        <Label1>{"Service"}</Label1>
+        <Display2>{textString}</Display2>
+        <Paragraph1>{bodyString}</Paragraph1>
+        <Input overrides={{ After }} placeholder="Search a bee yard..." />
+        <br />
+        <StyledLink href="#">Learn more about beekeeping</StyledLink>
+      </Cell>
+      <Cell span={[4, 4, 6]}>
+        <Inner h={80}> </Inner>
+      </Cell>
+    </Grid>
+  );
+}
+const Inner: React.FunctionComponent<{ h: number }> = ({
+  children,
+  h = 25,
+}) => {
+  const [css, theme] = useStyletron();
+  return (
+    <div
+      className={css({
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: theme.colors.accent200,
+        color: theme.colors.accent700,
+        padding: ".25rem",
+        height: h + "vh",
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import Head from "next/head";
 import { StatefulInput } from "baseui/input";
 import AppNavBar from "@components/molecules/AppNavBar/AppNavBar";
+import Hero from "@components/molecules/Hero";
 
 const Home: React.FC = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>Create Next App</title>
+        <title>Beeezy - Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AppNavBar />
-      home page
+      <Hero />
     </React.Fragment>
   );
 };
