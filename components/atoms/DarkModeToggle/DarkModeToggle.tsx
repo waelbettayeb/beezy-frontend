9600faa53b9228c5f6dc7827eefd29e87bd3ada6
WaelMohamedKhelil <khelilwael@gmail.com>
2020-07-07 13:12:43 +0100

    fix darkModeToggle

--- components/atoms/DarkModeToggle/DarkModeToggle.tsx
+++ components/atoms/DarkModeToggle/DarkModeToggle.tsx
@@ -1,18 +1,21 @@
 import React, { ReactElement } from "react";
 import { Checkbox, STYLE_TYPE } from "baseui/checkbox";
 import {} from "baseui";
+import { useTheme, THEME } from "@hooks/Theme";
 
 interface Props {}
 
 function DarkModeToggle({}: Props): ReactElement {
-  const [checkbox, setCheckbox] = React.useState(false);
+  const { theme, setTheme } = useTheme();
+  const [checkbox, setCheckbox] = React.useState(theme === THEME.Dark);
   return (
     <React.Fragment>
       <Checkbox
         checked={checkbox}
         onChange={(e) => {
-          const nextCheckbox = e.currentTarget.checked;
-          setCheckbox(nextCheckbox);
+          const value = e.currentTarget.checked;
+          setCheckbox(value);
+          value ? setTheme(THEME.Dark) : setTheme(THEME.Light);
         }}
         checkmarkType={STYLE_TYPE.toggle_round}
       />
