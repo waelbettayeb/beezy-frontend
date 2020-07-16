fceae29354b05fbf300b587afbd82e76a5e075f9
WaelMohamedKhelil <khelilwael@gmail.com>
2020-07-16 13:56:05 +0100

    add divider component

---
+++ src/components/atoms/Divider/Divider.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useStyletron } from "baseui";
+import { Label3 } from "baseui/typography";
+import { Block } from "baseui/block";
+
+export enum ORIENTATION {
+  left,
+  right,
+}
+export interface IProps {
+  orientation?: ORIENTATION;
+  children: any;
+}
+const Divider = (props: IProps) => {
+  const { orientation, children } = props;
+  const [css, theme] = useStyletron();
+
+  const isLeft = () => {
+    return orientation == ORIENTATION.left;
+  };
+  const isRight = () => {
+    return orientation == ORIENTATION.right;
+  };
+  const margin = theme.sizing.scale1000;
+  return (
+    <Block
+      display={"flex"}
+      alignContent={"space-around"}
+      marginTop={margin}
+      marginBottom={margin}
+    >
+      <hr
+        className={css({
+          ...theme.borders.border500,
+          width: isLeft() ? theme.sizing.scale700 : "100%",
+        })}
+      />
+      <Label3
+        width={"auto"}
+        marginLeft={theme.sizing.scale500}
+        marginRight={theme.sizing.scale500}
+      >
+        {children}
+      </Label3>
+      <hr
+        className={css({
+          ...theme.borders.border600,
+          width: isRight() ? theme.sizing.scale700 : "100%",
+        })}
+      />
+    </Block>
+  );
+};
+
+export default Divider;
