9c8e2709abe72acad556ef7054351149a7869ff3
WaelMohamedKhelil <khelilwael@gmail.com>
2020-07-22 11:18:36 +0100

    add avatar button

---
+++ src/components/atoms/AvatarButton/AvatarButton.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { Button } from "baseui/button";
+import { ChevronDown } from "baseui/icon";
+import { useAuth } from "@hooks/useAuth";
+import { Block } from "baseui/block";
+import { StatefulPopover } from "baseui/popover";
+import { useStyletron } from "baseui";
+import { StatefulMenu } from "baseui/menu";
+import DarkModeToggle from "../DarkModeToggle";
+import { Avatar } from "baseui/avatar";
+
+interface Props {}
+
+const AvatarButton = (props: Props) => {
+  const auth = useAuth();
+  const signOut = auth.signOut;
+  const [css, theme] = useStyletron();
+  const content = () => {
+    return (
+      <Block
+      //    margin={theme.sizing.scale900}
+      >
+        <StatefulMenu
+          items={[
+            { label: "Profile" },
+            { label: "My Services" },
+            {
+              label: (
+                <React.Fragment>
+                  <Block display={"flex"}>
+                    Dark mode
+                    <DarkModeToggle />
+                  </Block>
+                </React.Fragment>
+              ),
+            },
+            { label: "Log Out", onClick: signOut },
+          ]}
+          onItemSelect={({ item }) => {
+            if (item.onClick) item.onClick();
+          }}
+        />
+      </Block>
+    );
+  };
+  return (
+    <React.Fragment>
+      <StatefulPopover content={content} showArrow returnFocus autoFocus>
+        <Button
+          shape={"round"}
+          kind={"minimal"}
+          overrides={{
+            BaseButton: {
+              style: ({ $theme }) => {
+                return {
+                  padding: "0px",
+                };
+              },
+            },
+          }}
+        >
+          <Avatar name="Jane Doe" />
+        </Button>
+      </StatefulPopover>
+    </React.Fragment>
+  );
+};
+
+export default AvatarButton;
