ce26e45f5d0495a46cfb9ee594104a3b7887b4e9
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-07 17:00:22 +0100

    update ImageUploader

--- src/pages/services/create/create.tsx
+++ src/pages/services/create/create.tsx
@@ -3,9 +3,8 @@ import React from "react";
 import { FormControl } from "baseui/form-control";
 import { Input } from "baseui/input";
 import { Cell, Grid } from "baseui/layout-grid";
-import { Display4, Label1 } from "baseui/typography";
+import { Display4 } from "baseui/typography";
 import { Textarea } from "baseui/textarea";
-import { FileUploader } from "baseui/file-uploader";
 import dynamic from "next/dynamic";
 import { Block } from "baseui/block";
 import BackHomeNavBar from "@components/molecules/BackHomeNavBar /BackHomeNavBar ";
@@ -17,6 +16,8 @@ import {
 } from "src/mutations/gqlTypes/Upload";
 import { UploadFilesMutation } from "src/mutations/upload";
 import { useAuth } from "@hooks/useAuth";
+
+import ImagesUploader from "@components/molecules/ImagesUploader/ImagesUploader";
 
 const MapLocationPicker = dynamic(
   () => import("@components/molecules/MapLocationPicker"),
@@ -60,14 +61,15 @@ const create = () => {
             />
           </FormControl>
           <FormControl label="Photos">
-            <FileUploader
-              accept={"image/*"}
-              multiple
-              maxSize={5242880}
+            <ImagesUploader
+              images={images}
               errorMessage={errorMessage}
+              maxSize={5242880}
               onRetry={() => setErrorMessage("")}
+              progressMessage={loading ? `Uploading...` : ""}
               onDrop={(acceptedFiles, rejectedFiles) => {
-                if (rejectedFiles[0]) setErrorMessage("Images max size is 5mb");
+                if (rejectedFiles && rejectedFiles[0])
+                  setErrorMessage("Images max size is 5mb");
                 uploadFiles({
                   variables: {
                     files: acceptedFiles,
@@ -78,10 +80,8 @@ const create = () => {
                     setImages(images.concat(value.data.UploadFiles));
                     console.log(images);
                   })
-                  .catch((reason) => setErrorMessage(reason));
+                  .catch((reason) => console.log(reason));
               }}
-              // progressAmount is a number from 0 - 100 which indicates the percent of file transfer completed
-              progressMessage={loading ? `Uploading...` : ""}
             />
           </FormControl>
         </Cell>
