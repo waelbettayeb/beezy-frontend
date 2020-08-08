7f6222af7fe6ecbc12398c61745ce5d2e7f93944
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-08 18:38:15 +0100

    update appnavbar

--- src/components/molecules/AppNavBar/AppNavBar.tsx
+++ src/components/molecules/AppNavBar/AppNavBar.tsx
@@ -5,18 +5,20 @@ import {
   StyledNavigationList,
   StyledNavigationItem,
 } from "baseui/header-navigation";
-import { StyledLink } from "baseui/link";
 import { Button, KIND, SHAPE } from "baseui/button";
 import { Drawer } from "baseui/drawer";
 import LoginForm from "../LoginForm";
 import { useAuth } from "src/hooks/useAuth";
-import DarkModeToggle from "src/components/atoms/DarkModeToggle";
 import Logo from "src/components/atoms/Logo";
 import ChangeLocaleButton from "@components/atoms/ChangeLocaleButton";
 import AvatarButton from "@components/atoms/AvatarButton";
-import { useStyletron } from "baseui";
+import Router from "next/router";
 import { Theme } from "baseui/theme";
 import AddServiceButton from "../AddServiceButton";
+import { Block } from "baseui/block";
+import { Search } from "baseui/icon";
+import { Input } from "baseui/input";
+import { Grid, ALIGNMENT, Cell } from "baseui/layout-grid";
 interface Props {}
 
 const AppNavBar = (props: Props) => {
@@ -37,13 +39,63 @@ const AppNavBar = (props: Props) => {
         },
       }}
     >
-      <StyledNavigationList $align={ALIGN.left}>
-        <StyledNavigationItem>
-          <Logo />
-        </StyledNavigationItem>
-      </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.center}>
-        {/* <StyledNavigationItem>
+      <Block width="100%">
+        <Grid align={ALIGNMENT.center}>
+          <Cell span={[1, 0]}></Cell>
+          <Cell span={[0, 2, 4]}>
+            <Button
+              onClick={() => Router.push("/search")}
+              kind={"minimal"}
+              startEnhancer={<Search size="18px" />}
+            >
+              Search
+            </Button>
+          </Cell>
+          <Cell span={[2, 4, 4]}>
+            <Block
+              width="100%"
+              display={"flex"}
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Logo onClick={() => Router.push("/")} />
+            </Block>
+          </Cell>
+          <Cell span={[1, 2, 4]}>
+            <Block
+              width="100%"
+              display={"flex"}
+              flexDirection="row"
+              justifyContent="flex-end"
+              alignItems="center"
+            >
+              {auth.user ? (
+                <>
+                  <Block marginRight={"8px"}>
+                    <AddServiceButton />
+                  </Block>
+                  <AvatarButton />
+                </>
+              ) : (
+                <>
+                  <ChangeLocaleButton />
+                  <Button onClick={() => setIsOpen(true)}>Get started</Button>
+                  <Drawer
+                    isOpen={isOpen}
+                    autoFocus
+                    onClose={() => setIsOpen(false)}
+                  >
+                    <LoginForm onCompleted={() => setIsOpen(false)} />
+                  </Drawer>
+                </>
+              )}
+            </Block>
+          </Cell>
+        </Grid>
+      </Block>
+
+      {/* <StyledNavigationItem>
           <Button onClick={() => alert("click")} disabled kind={KIND.minimal}>
             Shop
           </Button>
@@ -58,40 +110,6 @@ const AppNavBar = (props: Props) => {
             Ressources
           </Button>
         </StyledNavigationItem> */}
-      </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.right}>
-        {auth.user ? (
-          <>
-            <StyledNavigationItem>
-              <AddServiceButton />
-            </StyledNavigationItem>
-            <StyledNavigationItem>
-              <AvatarButton />
-            </StyledNavigationItem>
-          </>
-        ) : (
-          <>
-            {/* <StyledNavigationItem>
-              <DarkModeToggle />
-            </StyledNavigationItem> */}
-            <StyledNavigationItem>
-              <ChangeLocaleButton />
-            </StyledNavigationItem>
-            <StyledNavigationItem>
-              <Button shape={SHAPE.pill} onClick={() => setIsOpen(true)}>
-                Get started
-              </Button>
-            </StyledNavigationItem>
-          </>
-        )}
-      </StyledNavigationList>
-      {auth.user ? (
-        ""
-      ) : (
-        <Drawer isOpen={isOpen} autoFocus onClose={() => setIsOpen(false)}>
-          <LoginForm onCompleted={() => setIsOpen(false)} />
-        </Drawer>
-      )}
     </HeaderNavigation>
   );
 };
