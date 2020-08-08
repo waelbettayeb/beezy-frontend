0980d0beade58fc52372fc5864e457b995a56a69
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-08 17:02:00 +0100

    refactor search tile

---
+++ src/components/organisms/SearchDrawer/ProfilesSearchTile.tsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { useStyletron } from "baseui";
+import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
+import { Label3 } from "baseui/typography";
+import { ListItem, ListItemLabel, ARTWORK_SIZES } from "baseui/list";
+
+import { SearchProvider, WithSearch } from "@elastic/react-search-ui";
+import { Avatar } from "baseui/avatar";
+import { ChevronRight } from "baseui/icon";
+import { StyledLink } from "baseui/link";
+
+import { Block } from "baseui/block";
+
+interface Props {
+  searchedTerm: string;
+}
+
+const ProfilesSearchTile = (props: Props) => {
+  const [css, theme] = useStyletron();
+  const connector = new AppSearchAPIConnector({
+    searchKey: "search-v8bawehypykhcvry7p2soobg",
+    engineName: "users",
+    endpointBase: "http://127.0.0.1:3002",
+    cacheResponses: true,
+  });
+  const config = {
+    apiConnector: connector,
+  };
+  return (
+    <SearchProvider config={config}>
+      <WithSearch
+        mapContextToProps={({
+          searchTerm,
+          setSearchTerm,
+          results,
+          setResultsPerPage,
+          setCurrent,
+        }) => ({
+          searchTerm,
+          setSearchTerm,
+          results,
+          setResultsPerPage,
+          setCurrent,
+        })}
+      >
+        {({
+          searchTerm,
+          setSearchTerm,
+          results,
+          setResultsPerPage,
+          setCurrent,
+        }) => {
+          setSearchTerm(props.searchedTerm);
+          setResultsPerPage(3);
+          setCurrent(1);
+          return (
+            <ul
+              className={css({
+                width: "100%",
+                paddingLeft: 0,
+                paddingRight: 0,
+              })}
+            >
+              {!results.length ? (
+                <Block
+                  width="100%"
+                  display={"flex"}
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Label3 color={theme.colors.contentInverseSecondary}>
+                    No data
+                  </Label3>
+                </Block>
+              ) : (
+                <div
+                  className={css({
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignContent: "center",
+                  })}
+                >
+                  <Label3>People</Label3>
+                  <StyledLink href="#">Show more</StyledLink>
+                </div>
+              )}
+              {results.map((r) => (
+                <ListItem
+                  endEnhancer={() => <ChevronRight />}
+                  artwork={() => (
+                    <Avatar
+                      name={`${r.firstname.raw} ${r.lastname.raw}`}
+                      size="scale1200"
+                    />
+                  )}
+                  artworkSize={ARTWORK_SIZES.SMALL}
+                >
+                  <ListItemLabel description={r.email.raw}>
+                    {`${r.firstname.raw} ${r.lastname.raw}`}
+                  </ListItemLabel>
+                </ListItem>
+              ))}
+            </ul>
+          );
+        }}
+      </WithSearch>
+    </SearchProvider>
+  );
+};
+
+export default ProfilesSearchTile;
