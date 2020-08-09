25715fbb748265c0e94fc8db185c2c9d462c69d6
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-09 18:27:52 +0100

    Refactor mapLocationPicker

--- src/components/molecules/MapLocationPicker/MapLocationPicker.tsx
+++ src/components/molecules/MapLocationPicker/MapLocationPicker.tsx
@@ -1,34 +1,33 @@
 import React from "react";
-import { Map, TileLayer, Marker } from "react-leaflet";
+import { Map, TileLayer, Marker, Circle } from "react-leaflet";
 import { Label1 } from "baseui/typography";
 interface Props {
   lat: number;
   lng: number;
   zoom?: number;
-  onViewportChange: (lat, lng) => void;
+  radius?: number;
+  onViewportChange?: (lat?, lng?, zoom?) => void;
+  onViewportChanged?: (lat?, lng?, zoom?) => void;
+  onLoad?: (lat?, lng?, zoom?) => void;
+}
+
+export function simpleReverseGeocoding(position): Promise<Response> {
+  const url = `http://nominatim.openstreetmap.org/reverse?format=json&lon=${position.lng}&lat=${position.lat}`;
+  return fetch(url);
 }
 
 const MapLocationPicker = (props: Props) => {
-  const { lat, lng, onViewportChange } = props;
-  const [zoom, setZoom] = React.useState(props.zoom | 8);
-  const [address, setAddress] = React.useState("");
-
+  const { lat, lng, onViewportChange, onViewportChanged, radius } = props;
+  const [zoom, setZoom] = React.useState(props.zoom | 7);
   const handleViewportchange = (e) => {
-    onViewportChange(e.center[0], e.center[1]);
+    setZoom(e.zoom);
+    onViewportChange && onViewportChange(e.center[0], e.center[1]);
   };
-  function simpleReverseGeocoding(position) {
-    const url = `http://nominatim.openstreetmap.org/reverse?format=json&lon=${position.lng}&lat=${position.lat}`;
-    fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .catch(function (error) {
-        setAddress("");
-      })
-      .then(function (json) {
-        setAddress(json.display_name);
-      });
-  }
+  const handleViewportchanged = (e) => {
+    setZoom(e.zoom);
+    onViewportChanged && onViewportChanged(e.center[0], e.center[1]);
+  };
+
   return (
     <>
       <Map
@@ -38,20 +37,17 @@ const MapLocationPicker = (props: Props) => {
           handleViewportchange(e);
         }}
         onViewportChanged={(e) => {
-          simpleReverseGeocoding({ lat, lng });
+          handleViewportchanged(e);
         }}
-        onload={() => {
-          simpleReverseGeocoding({ lat, lng });
-        }}
+        animate={false}
       >
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
         <Marker position={{ lat, lng }}></Marker>
+        {radius && <Circle center={{ lat, lng }} radius={radius}></Circle>}
       </Map>
-      <Label1>{address}</Label1>
     </>
   );
 };
