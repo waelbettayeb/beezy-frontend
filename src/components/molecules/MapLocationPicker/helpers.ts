5f522afbfb9d0c2e054a8e4105e7a8d1473324d2
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-11 20:49:33 +0100

    secure simpleReverseGeocoding

--- src/components/molecules/MapLocationPicker/helpers.ts
+++ src/components/molecules/MapLocationPicker/helpers.ts
@@ -1,5 +1,5 @@
 export function simpleReverseGeocoding(lat = 0, lng = 0) {
-  const url = `http://nominatim.openstreetmap.org/reverse?format=json&lon=${lng}&lat=${lat}`;
+  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lon=${lng}&lat=${lat}`;
   return fetch(url).then(function (response) {
     return response.json();
   });
