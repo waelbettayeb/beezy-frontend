fe7c70c981f454411ae9c6e45bf42db505035760
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-11 21:51:39 +0100

    fix sign in navbar

--- src/pages/auth/signin/signin.tsx
+++ src/pages/auth/signin/signin.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import SignupForm from "@components/molecules/SignupForm";
 import { Grid, Cell, ALIGNMENT } from "baseui/layout-grid";
 import { Block } from "baseui/block";
-import Logo from "@components/atoms/Logo";
 import { useStyletron } from "baseui";
 import { useAuth } from "@hooks/useAuth";
 import Router from "next/router";
 import LoginForm from "@components/molecules/LoginForm";
+import BackHomeNavBar from "@components/molecules/BackHomeNavBar /BackHomeNavBar ";
 
 interface Props {}
 
@@ -16,18 +15,8 @@ const SignIn = (props: Props) => {
   if (auth.user) Router.push("/");
   return (
     <React.Fragment>
+      <BackHomeNavBar />
       <Grid align={ALIGNMENT.center}>
-        <Cell span={[4, 8, 12]}>
-          <Block
-            display={"flex"}
-            justifyContent={"center"}
-            marginTop={theme.sizing.scale800}
-            marginBottom={theme.sizing.scale800}
-            width={"100%"}
-          >
-            <Logo onClick={() => Router.push("/")} />
-          </Block>
-        </Cell>
         <Cell span={[0, 0, 3]}>
           <Block minHeight={"80vh"}></Block>
         </Cell>
