950585fefe40827bbd3c40341297ed6c5ee416ae
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-13 15:22:11 +0100

    add router to listing

--- src/components/organisms/SearchDrawer/ListingsSearchTile.tsx
+++ src/components/organisms/SearchDrawer/ListingsSearchTile.tsx
@@ -3,6 +3,7 @@ import { useStyletron } from "baseui";
 import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
 import {
   Caption1,
+  Caption2,
   Label3,
   LabelSmall,
   LabelXSmall,
@@ -15,8 +16,9 @@ import { SearchProvider, WithSearch } from "@elastic/react-search-ui";
 import { StyledLink } from "baseui/link";
 
 import { Block } from "baseui/block";
-import { Card, StyledHeaderImage, StyledThumbnail } from "baseui/card";
+import Router from "next/router";
 import { Cell, Grid } from "baseui/layout-grid";
+import TimeAgo from "@components/atoms/TimeAgo";
 
 interface Props {
   searchedTerm: string;
@@ -101,33 +103,42 @@ const ListingsSearchTile = (props: Props) => {
               <Grid gridMargins={10} gridGaps={10} gridGutters={10}>
                 {results.map((r) => (
                   <Cell span={[2, 4, 4]}>
-                    <Block
-                      display="flex"
-                      flexDirection="column"
-                      alignItems={"center"}
-                      width="100%"
-                      overrides={{
-                        Block: {
-                          style: {
-                            ...theme.borders.border600,
-                            borderRadius: theme.borders.surfaceBorderRadius,
-                            textOverflow: "ellipsis",
-                          },
-                        },
-                      }}
+                    <div
+                      onClick={() =>
+                        Router.push(
+                          "/listing/[id]",
+                          `/listing/${r.id.raw.substring(8)}`
+                        )
+                      }
                     >
-                      <img
-                        src={
-                          "https://s3.beeesy.com/beeesy/listings/51/thumbnail_Group_3_572c45fbd4.png"
-                        }
-                        className={css({
-                          width: "100%",
-                        })}
-                      />
-                      <Block padding={theme.sizing.scale500}>
-                        <Paragraph4>{r.title.raw}</Paragraph4>
+                      <Block
+                        display="flex"
+                        flexDirection="column"
+                        alignItems={"center"}
+                        width="100%"
+                        overrides={{
+                          Block: {
+                            style: {
+                              ...theme.borders.border600,
+                              borderRadius: theme.borders.surfaceBorderRadius,
+                              textOverflow: "ellipsis",
+                            },
+                          },
+                        }}
+                      >
+                        <img
+                          src={
+                            "https://s3.beeesy.com/beeesy/listings/51/thumbnail_Group_3_572c45fbd4.png"
+                          }
+                          className={css({
+                            width: "100%",
+                          })}
+                        />
+                        <Block padding={theme.sizing.scale500}>
+                          <Paragraph4>{r.title.raw}</Paragraph4>
+                        </Block>
                       </Block>
-                    </Block>
+                    </div>
                   </Cell>
                 ))}
               </Grid>
