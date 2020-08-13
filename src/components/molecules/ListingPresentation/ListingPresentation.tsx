65255e5a7bcb97810e178e205da9c27a4812b74e
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-13 12:36:08 +0100

    add listing component

---
+++ src/components/molecules/ListingPresentation/ListingPresentation.tsx
@@ -0,0 +1,102 @@
+import React, { ReactElement } from "react";
+import { Grid, Cell, ALIGNMENT, BEHAVIOR } from "baseui/layout-grid";
+import {
+  Paragraph1,
+  DisplaySmall,
+  LabelLarge,
+  ParagraphSmall,
+} from "baseui/typography";
+import { useStyletron } from "baseui";
+import { Search } from "baseui/icon";
+import { Input, SIZE } from "baseui/input";
+import { StyledLink } from "baseui/link";
+import { Block } from "baseui/block";
+import { FormattedMessage } from "react-intl";
+import SearchDrawer from "@components/organisms/SearchDrawer";
+import {
+  IListingPayload,
+  IListingVariables,
+} from "@graphql/queries/gqlTypes/listing";
+import { listingQuery } from "@graphql/queries/listing";
+import { useQuery } from "react-apollo";
+import { Spinner } from "baseui/spinner";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("@components/atoms/Map"), {
+  ssr: false,
+});
+interface Props {
+  id: string;
+}
+
+const ListingPresentation: React.FC<Props> = (props) => {
+  const { id } = props;
+  const [css, _theme] = useStyletron();
+  const { data, error, loading } = useQuery<IListingPayload, IListingVariables>(
+    listingQuery,
+    {
+      variables: { id },
+    }
+  );
+  const { listing } = !loading && data;
+  return (
+    <>
+      <Inner h={100}>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <Grid
+              overrides={{
+                Grid: {
+                  style: ({ $theme }) => ({
+                    height: "100%",
+                  }),
+                },
+              }}
+            >
+              <Cell span={[4, 4, 6]}></Cell>
+              <Cell span={[4, 4, 6]}>
+                <LabelLarge>{listing.title}</LabelLarge>
+                <ParagraphSmall>{listing.description}</ParagraphSmall>
+                <Block height={"50vh"}>
+                  <Map
+                    lat={listing.location.latitude}
+                    lng={listing.location.longitude}
+                    markers={[
+                      {
+                        lat: listing.location.latitude,
+                        lng: listing.location.longitude,
+                      },
+                    ]}
+                  />
+                </Block>
+              </Cell>
+            </Grid>
+          </>
+        )}
+      </Inner>
+    </>
+  );
+};
+const Inner: React.FunctionComponent<{ h: number }> = ({
+  children,
+  h = 25,
+}) => {
+  const [css, theme] = useStyletron();
+  return (
+    <div
+      className={css({
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: theme.colors.accent700,
+        padding: ".25rem",
+        height: h + "vh",
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+export default ListingPresentation;
