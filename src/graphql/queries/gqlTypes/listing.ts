91eb8066532b4cb13b5bfc28099b1e5523c6e30e
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-13 11:58:39 +0100

    add listing query

---
+++ src/graphql/queries/gqlTypes/listing.ts
@@ -0,0 +1,18 @@
+export interface IListingPayload {
+  listing: {
+    id: string;
+    title: string;
+    description: string;
+    location: {
+      longitude: number;
+      latitude: number;
+    };
+    images: {
+      url: string;
+    };
+    created_at: Date;
+  };
+}
+export interface IListingVariables {
+  id: string;
+}
