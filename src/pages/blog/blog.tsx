95e5b1ff040d0232877a8bbfb08df319a3bf96f0
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-17 11:43:59 +0100

    create blog page

---
+++ src/pages/blog/blog.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import SearchNavBar from "@components/molecules/SearchNavBar/SearchNavBar";
+import { useStyletron } from "baseui";
+import ListingsSearchTile from "@components/molecules/ListingsSearchTile";
+import { Cell, Grid } from "baseui/layout-grid";
+import { Block } from "baseui/block";
+import { Tabs, Tab } from "baseui/tabs";
+
+import AppNavBar from "@components/molecules/AppNavBar/AppNavBar";
+import Router from "next/router";
+import { Avatar } from "baseui/avatar";
+import {
+  Caption1,
+  HeadingXSmall,
+  LabelLarge,
+  LabelMedium,
+  Paragraph1,
+  ParagraphXSmall,
+} from "baseui/typography";
+import Divider from "@components/atoms/Divider";
+import { ORIENTATION } from "@components/atoms/Divider/Divider";
+import Footer from "@components/organisms/Footer";
+import { useQuery } from "@apollo/react-hooks";
+import { userQuery } from "@graphql/queries/user";
+import Link from "next/link";
+
+const Blog = () => {
+  const [css, theme] = useStyletron();
+
+  return (
+    <>
+      <Block
+        height="100vh"
+        width={"100%"}
+        display={"flex"}
+        flexDirection={"column"}
+      >
+        <AppNavBar />
+      </Block>
+      <Footer />
+    </>
+  );
+};
+
+export default Blog;
