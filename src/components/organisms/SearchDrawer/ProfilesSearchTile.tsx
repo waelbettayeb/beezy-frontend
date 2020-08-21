b1f8632b55e20ca8a0cf179242cb079fe33e6766
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-21 14:44:29 +0100

    update people list

--- src/components/organisms/SearchDrawer/ProfilesSearchTile.tsx
+++ src/components/organisms/SearchDrawer/ProfilesSearchTile.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 import { useStyletron } from "baseui";
-import { Label3 } from "baseui/typography";
+import {
+  Caption2,
+  Label3,
+  LabelMedium,
+  LabelXSmall,
+  Paragraph4,
+} from "baseui/typography";
 import { ListItem, ListItemLabel, ARTWORK_SIZES } from "baseui/list";
 
 import { Avatar } from "baseui/avatar";
@@ -9,6 +15,8 @@ import { StyledLink } from "baseui/link";
 
 import { Block } from "baseui/block";
 import Router from "next/router";
+import TimeAgo from "@components/atoms/TimeAgo";
+import { Card, StyledAction, StyledBody } from "baseui/card";
 
 interface Props {
   results: any[];
@@ -44,34 +52,49 @@ const ProfilesSearchTile = (props: Props) => {
             flexDirection: "row",
             justifyContent: "space-between",
             alignContent: "center",
+            marginBottom: theme.sizing.scale200,
           })}
         >
           <Label3>People</Label3>
-          <StyledLink href="#">Show more</StyledLink>
+          {/* <StyledLink href="#">Show more</StyledLink> */}
         </div>
       )}
-      {results &&
-        results.map((r) => (
-          <div
-            onClick={() => Router.push("/profile/[pid]", `/profile/${r.id}`)}
-          >
-            <ListItem
-              endEnhancer={() => <ChevronRight />}
-              artwork={() => (
+      <Block display="flex" overflow={"auto"} width={"100%"}>
+        {results &&
+          results.map((r) => (
+            <div
+              onClick={() => Router.push("/profile/[pid]", `/profile/${r.id}`)}
+            >
+              <Block
+                height={theme.sizing.scale3200}
+                width={theme.sizing.scale2400}
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"space-around"}
+                overrides={{
+                  Block: {
+                    style: ({ $theme }) => {
+                      return {
+                        ...theme.borders.border600,
+                        padding: theme.sizing.scale300,
+                        marginRight: theme.sizing.scale200,
+                      };
+                    },
+                  },
+                }}
+              >
                 <Avatar
                   src={r.avatar[0]?.file.url}
                   name={`${r.firstName} ${r.lastName}`}
-                  size="scale1200"
+                  size="scale1400"
                 />
-              )}
-              artworkSize={ARTWORK_SIZES.SMALL}
-            >
-              <ListItemLabel description={r.email}>
-                {`${r.firstName} ${r.lastName}`}
-              </ListItemLabel>
-            </ListItem>
-          </div>
-        ))}
+
+                <LabelXSmall>{`${r.firstName} ${r.lastName}`}</LabelXSmall>
+              </Block>
+            </div>
+          ))}
+      </Block>
     </ul>
   );
 };
