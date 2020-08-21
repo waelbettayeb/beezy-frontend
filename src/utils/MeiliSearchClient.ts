8e001c984102494da6cc0ce0184e0dc14241b4e7
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-21 12:15:56 +0100

    add search key and endpoint env var

--- src/utils/MeiliSearchClient.ts
+++ src/utils/MeiliSearchClient.ts
@@ -1,6 +1,7 @@
 import MeiliSearch from "meilisearch";
 
 const client = new MeiliSearch({
-  host: "http://127.0.0.1:7700/",
+  host: `${process.env.NEXT_PUBLIC_SEARCH_ENDPOINT}`,
+  apiKey: `${process.env.NEXT_PUBLIC_SEARCH_KEY}`,
 });
 export default client;
