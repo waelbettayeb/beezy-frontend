0af95db8b5db101809e50aea5201d3ee796c8729
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-21 18:53:41 +0100

    fix callback page

--- src/pages/auth/[provider].tsx
+++ src/pages/auth/[provider].tsx
@@ -6,28 +6,41 @@ import { setAuthToken } from "@utils/auth";
 import LoadingScreen from "@components/molecules/LoadingScreen";
 import { useAuth } from "@hooks/useAuth";
 import { ParagraphLarge } from "baseui/typography";
+import BackHomeNavBar from "@components/molecules/BackHomeNavBar /BackHomeNavBar ";
 
 const AuthCallbackPage = (props: Props) => {
   const [text, setText] = React.useState("Loading...");
   const { access_token, provider } = Router.query;
   const auth = useAuth();
-
   React.useEffect(() => {
     // Successfully logged with the provider
     // Now logging with strapi by using the access_token (given by the provider) in props.location.search
     fetch(
       `${process.env.NEXT_PUBLIC_API_URI}/auth/${provider}/callback?access_token=${access_token}`
     )
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error(`Couldn't login to Beeesy. Status: ${res.status}`);
+        }
+        return res;
+      })
       .then((res) => res.json())
       .then((res) => {
         setAuthToken(res.jwt as string, () => {
-          auth.getUser();
-          const { data, error, loading } = auth.fetchUser();
           Router.push("/");
         });
+      })
+      .catch((err) => {
+        console.log(err);
+        setText("An error occured.");
       });
   }, [provider]);
-  return <ParagraphLarge>{text}</ParagraphLarge>;
+  return (
+    <>
+      <BackHomeNavBar />
+      <ParagraphLarge>{text}</ParagraphLarge>
+    </>
+  );
 };
 
 export default AuthCallbackPage;
