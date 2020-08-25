645cea1e0346951c122e462d2f7d03242763cf77
Wael Mohamed BETTAYEB <bettayebwael@gmail.com>
2020-08-25 18:22:20 +0100

    refactor provider callback page

--- src/pages/auth/[provider].tsx
+++ src/pages/auth/[provider].tsx
@@ -8,19 +8,29 @@ import { useAuth } from "@hooks/useAuth";
 import { ParagraphLarge } from "baseui/typography";
 import BackHomeNavBar from "@components/molecules/BackHomeNavBar /BackHomeNavBar ";
 import { toaster } from "baseui/toast";
+import { useIntl } from "react-intl";
 
 const AuthCallbackPage = (props: Props) => {
   const [text, setText] = React.useState("Loading...");
   const { access_token, provider } = Router.query;
   const [loading, setSetloading] = React.useState(true);
-
+  const intl = useIntl();
   React.useEffect(() => {
     fetch(
       `${process.env.NEXT_PUBLIC_API_URI}/auth/${provider}/callback?access_token=${access_token}`
     )
       .then((res) => {
         if (res.status !== 200) {
-          throw new Error(`Couldn't login to Beeesy. Status: ${res.status}`);
+          throw new Error(
+            intl.formatMessage(
+              {
+                defaultMessage: `Couldn't login to Beeesy. Status: {status}`,
+              },
+              {
+                status: res.status,
+              }
+            )
+          );
         }
         return res;
       })
@@ -28,12 +38,21 @@ const AuthCallbackPage = (props: Props) => {
       .then((res) => {
         localStorage.setItem("token", res.jwt);
         Router.push("/");
-        toaster.info("You are successfully logged in", {});
+        toaster.info(
+          intl.formatMessage({
+            defaultMessage: "You are successfully logged in",
+          }),
+          {}
+        );
       })
       .catch((err) => {
         setSetloading(false);
         console.log(err);
-        setText("An error occured.");
+        setText(
+          intl.formatMessage({
+            defaultMessage: "An error occured.",
+          })
+        );
       });
   }, [provider]);
   return (
